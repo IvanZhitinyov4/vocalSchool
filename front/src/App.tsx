82c11a6e5e8a706339e7fd02465e383ca06c2bde
mrnavi <misternavi5@gmail.com>
2024-07-18 22:18:26 +1000

    changed js files on ts

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavMenu} from "./components/NavMenu";
+import {Routes, Route} from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/NavMenu.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// @ts-ignore
+import {NavLink} from "react-router-dom";
+
+import "./NavMenu.css";
+
+export const NavMenu = () => {
+    <React.Fragment>
+        <div className="menu">
+            <div className="title">
+                <NavLink to={'/'}>Школа Вокала</NavLink>
+            </div>
+        </div>
+    </React.Fragment>
+};
